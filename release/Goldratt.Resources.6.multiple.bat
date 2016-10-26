1d558cf463c21ff36d57eb10bd724c785c4dd1d0
artempolyvyanyy <artem.polyvyanyy@unimelb.edu.au>
2016-10-26 23:26:59 +0000

    adjusted examples
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/SelectivePRepair/Trunk@31650 a68f48d4-8d00-424f-b812-3f22a534a02a

--- release/Goldratt.Resources.6.multiple.bat
+++ release/Goldratt.Resources.6.multiple.bat
@@ -1,2 +1,2 @@
-java -jar ImpactDrivenPMRepair-TOSEM.jar -p=Fig.1.pnml -x=L3.xes -r=6 -c=name -s -f -g -m
+java -jar ImpactDrivenPMRepair-TOSEM.jar -p=Fig.2.pnml -x=L3.xes -r=6 -c=name -s -f -g -m
 pause
